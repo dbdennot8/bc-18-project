7c321367d88fd03a0a639784485930728709c665
dbdennot8 <dennisbmithamo@gmail.com>
2017-05-17 00:35:03 +0300

    Parent class for the room tye options

--- Room.py
+++ Room.py
@@ -1,11 +1,12 @@
-"""Defines the blueprint for creating an instance of room"""
-
-
 class Room(object):
-	def __init__(self, room_type, room_name, occupants):
-		self.room_type = room_type
-		self.room_name = room_name
-		self.occupants = occupants
+    """Defines the blueprint for creating an instance of room"""
+    def __init__(self, room_type, room_name, max_occupants, occupants=[]):
+        self.room_type = room_type
+        self.room_name = room_name
+        self.max_occupants = max_occupants
+        self.occupants = occupants
 
-	def allocate_room(self):
-		pass
+    def add_occupant(self, first_name):
+        if len(self.occupants) < self.max_occupants:
+            self.occupants.append(first_name)
+
