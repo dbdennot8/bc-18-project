cd7dcd0a2b1b375ef1e3f3b49ac842e8719503b5
dbdennot8 <dennisbmithamo@gmail.com>
2017-05-17 00:36:20 +0300

    Parent class for the options for person

--- Staff.py
+++ Staff.py
@@ -1,9 +1,12 @@
-"""Sub-class Staff which inherits from class Person
-Blueprint for introducing a new Staff to the system."""
+from person import Person
+
 
 class Staff(Person):
-    def __init__(self, person_type, first_name, surname, wants_accomodation):
-        self.person_type = "Staff"
+    """Sub-class Staff which inherits from class Person
+    Blueprint for introducing a new Staff to the system."""
+    def __init__(self, person_type, first_name, surname, wants_accommodation="N"):
+        super(Staff, self).__init__(person_type, first_name, surname)
+        self.person_type = "staff"
         self.first_name = first_name
         self.surname = surname
-        self.wants_accomodation = "N"
+        self.wants_accommodation = wants_accommodation
