120aeeab56fc627be8664a1cf8ebf0a9d9520c1c
dbdennot8 <dennisbmithamo@gmail.com>
2017-05-17 00:38:56 +0300

    Adds a new person, either a fellow or staff

--- person.py
+++ person.py
@@ -1,10 +1,8 @@
-"""This defines the blueprint for creating an instance of person"""
-
-
 class Person(object):
-    def __init__(self, person_type, first_name, surname, wants_accomodation):
-    self.person_type = person_type
-    self.first_name = first_name
-    self.surname = surname
-    self.wants_accomodation = "N"
+    """This defines the blueprint for creating an instance of person"""
+    def __init__(self, person_type, first_name, surname, wants_accomodation="N"):
+        self.person_type = person_type
+        self.first_name = first_name
+        self.surname = surname
+        self.wants_accomodation = wants_accomodation
 
