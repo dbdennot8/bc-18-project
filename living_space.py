ce867f6da771777909c59417f7dadba39cfd7b90
dbdennot8 <dennisbmithamo@gmail.com>
2017-05-17 00:37:39 +0300

    Creates an instance of living space

--- living_space.py
+++ living_space.py
@@ -1,8 +1,12 @@
-"""Defines the blueprint for creating a new LivingSpace"""
-
 from Room import Room
+
+
 class LivingSpace(Room):
-    def __init__(self, room_name, room_type, max_occupants):
+    """Defines the blueprint for creating a new Living Space"""
+    def __init__(self, room_type, room_name, max_occupants=4, occupants=[]):
+        super(LivingSpace, self).__init__(room_type, room_name, max_occupants=4, occupants=[])
+        self.room_type = "living space"
         self.room_name = room_name
-        self.room_type = "office"
-        self.max_occupants = 4
+        self.max_occupants = max_occupants
+        self.occupants = occupants
+
