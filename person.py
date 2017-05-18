a098bfa355165364875795c51e6c5e7737846e06
dbdennot8 <dennisbmithamo@gmail.com>
2017-05-18 19:33:08 +0300

    Delete from master branch

--- person.py
+++
@@ -1,8 +0,0 @@
-class Person(object):
-    """This defines the blueprint for creating an instance of person"""
-    def __init__(self, person_type, first_name, surname, wants_accomodation="N"):
-        self.person_type = person_type
-        self.first_name = first_name
-        self.surname = surname
-        self.wants_accomodation = wants_accomodation
-
